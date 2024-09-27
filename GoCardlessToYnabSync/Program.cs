b2f3ba6ffd388f394bec1d1e31cf634e9bfffd7e
Nikola Invernizzi <56538696+NikolaInvernizzi@users.noreply.github.com>
2024-09-27 10:09:07 +0200

    Update Program.cs

--- GoCardlessToYnabSync/Program.cs
+++ GoCardlessToYnabSync/Program.cs
@@ -18,26 +18,11 @@ var host = new HostBuilder()
         services.AddTransient<CosmosDbService>();
         services.AddTransient<MailService>();
 
-
         services.Configure<GoCardlessOptions>(hostContext.Configuration.GetSection(GoCardlessOptions.GoCardless));
         services.Configure<YnabOptions>(hostContext.Configuration.GetSection(YnabOptions.Ynab));
         services.Configure<FunctionUriOptions>(hostContext.Configuration.GetSection(FunctionUriOptions.FunctionUris));
         services.Configure<CosmosDbOptions>(hostContext.Configuration.GetSection(CosmosDbOptions.CosmosDb));
         services.Configure<SmptOptions>(hostContext.Configuration.GetSection(SmptOptions.Smpt));
-
-
-        //services.AddOptions<FunctionUriOptions>().Configure<IConfiguration>((settings, configuration) => {
-        //    hostContext.Configuration.GetSection(FunctionUriOptions.FunctionUris);
-        //});
-        //services.AddOptions<GoCardlessOptions>().Configure<IConfiguration>((settings, configuration) => {
-        //    hostContext.Configuration.GetSection(GoCardlessOptions.GoCardless);
-        //});
-        //services.AddOptions<SmptOptions>().Configure<IConfiguration>((settings, configuration) => {
-        //    hostContext.Configuration.GetSection(SmptOptions.Smpt);
-        //});
-        //services.AddOptions<YnabOptions>().Configure<IConfiguration>((settings, configuration) => {
-        //    hostContext.Configuration.GetSection(YnabOptions.Ynab);
-        //});
     })
     .ConfigureAppConfiguration((context, builder) =>
     {
@@ -47,6 +32,5 @@ var host = new HostBuilder()
                .AddUserSecrets<Program>()
                .AddEnvironmentVariables();
     }).Build();
-
 
 host.Run();
